64252d9cf051c9e470ade32829114a11301a528e
dpotikan <dpotikan@eng.cmu.ac.th>
2024-08-25 12:00:54 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,206 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+export default function RegisterForm() {
+  const [fname, setFname] = useState("");
+  const [fnameError, setFnameError] = useState(false);
+  const [lname, setLname] = useState("");
+  const [plan, setPlan] = useState("");
+  const [gender, setGender] = useState("male");
+  const [buyBottle, setBuyBottle] = useState(false);
+  const [buyShoes, setBuyShoes] = useState(false);
+  const [buyCap, setBuyCap] = useState(false);
+
+  // ----------------------------------------------------------------
+
+  const inputFnameOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFnameError(false);
+    setFname(event.target.value);
+  };
+
+  const inputLnameOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLname(event.target.value);
+  };
+
+  const selectPlanOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setPlan(event.target.value);
+  };
+
+  const radioGenderMaleOnChange = () => {
+    setGender("male");
+  };
+
+  const radioGenderFemaleOnChange = () => {
+    setGender("female");
+  };
+
+  const cbBuyBottleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyBottle(event.target.checked);
+  };
+
+  const cbBuyShoesOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyShoes(event.target.checked);
+  };
+
+  const cbBuyCapOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyCap(event.target.checked);
+  };
+
+  // ----------------------------------------------------------------
+
+  const computeTotalPayment = () => {
+    let total = 0;
+    if (plan === "funrun") total += 500;
+    if (plan === "mini") total += 800;
+    if (plan === "half") total += 1200;
+    if (plan === "full") total += 1500;
+    if (buyBottle) total += 200;
+    if (buyShoes) total += 600;
+    if (buyCap) total += 400;
+
+    return total;
+  };
+
+  // ----------------------------------------------------------------
+
+  const registerBtnOnClick = () => {
+    let fnameOk = true;
+    if (fname === "") {
+      fnameOk = false;
+      setFnameError(true);
+    }
+
+    if (fnameOk) {
+      alert(
+        `Registration complete. Please pay money for ${computeTotalPayment().toLocaleString()} THB.`
+      );
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <div className="mx-auto vstack gap-3" style={{ width: "400px" }}>
+      <h3 className="text-center fst-italic my-4">Register CMU Marathon 🏃‍♂️</h3>
+      {/* First name & Last name */}
+      <div className="d-flex gap-2">
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <label className="form-label">First name</label>
+          <input
+            className={"form-control" + (fnameError ? " is-invalid" : "")}
+            onChange={inputFnameOnChange}
+            value={fname}
+          />
+          <div className="invalid-feedback">Invalid first name</div>
+        </div>
+        <div>
+          <label className="form-label">Last name</label>
+          <input
+            className="form-control"
+            onChange={inputLnameOnChange}
+            value={lname}
+          />
+          <div className="invalid-feedback">Invalid last name</div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Running Plan */}
+      <div>
+        <label className="form-label">Plan</label>
+        <select
+          className="form-select"
+          onChange={selectPlanOnChange}
+          value={plan}
+        >
+          <option value="">Please select..</option>
+          <option value="funrun">Fun run 5.5 Km (500 THB)</option>
+          <option value="mini">Mini Marathon 10 Km (800 THB)</option>
+          <option value="half">Half Marathon 21 Km (1,200 THB)</option>
+          <option value="full">Full Marathon 42.195 Km (1,500 THB)</option>
+        </select>
+        <div className="invalid-feedback">Please select a Plan</div>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Gender */}
+      <div>
+        <label className="form-label">Gender</label>
+        <div>
+          <input
+            className="me-2 form-check-input"
+            type="radio"
+            onChange={radioGenderMaleOnChange}
+            checked={gender === "male"}
+          />
+          Male 👨
+          <input
+            className="mx-2 form-check-input"
+            type="radio"
+            onChange={radioGenderFemaleOnChange}
+            checked={gender === "female"}
+          />
+          Female 👩
+          {/* To show error when user did not select gender, */}
+          {/* We just have to render the div below (Not using is-invalid bootstrap class) */}
+          {/* <div className="text-danger">Please select gender</div> */}
+        </div>
       </div>
-    </main>
+
+      {/* Extra Items */}
+      <div>
+        <label className="form-label">Extra Item(s)</label>
+        <div>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onChange={cbBuyBottleOnChange}
+            checked={buyBottle}
+          />{" "}
+          <label className="form-check-label">Bottle 🍼 (200 THB)</label>
+        </div>
+        <div>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onChange={cbBuyShoesOnChange}
+            checked={buyShoes}
+          />{" "}
+          <label className="form-check-label">Shoes 👟 (600 THB)</label>
+        </div>
+        <div>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onChange={cbBuyCapOnChange}
+            checked={buyCap}
+          />{" "}
+          <label className="form-check-label">Cap 🧢 (400 THB)</label>
+        </div>
+      </div>
+
+      <div className="alert alert-primary" role="alert">
+        Promotion📢 Buy all items to get 20% Discount
+      </div>
+
+      {/* Total Payment */}
+      <div>
+        Total Payment : {computeTotalPayment().toLocaleString()} THB
+        {/* Render below element conditionally when user get 20% discount */}
+        {/* <span className="text-success d-block">(20% Discounted)</span> */}
+      </div>
+
+      {/* Terms and conditions */}
+      <div>
+        <input className="me-2" type="checkbox" />I agree to the terms and
+        conditions
+      </div>
+
+      {/* Register Button */}
+      <button
+        className="btn btn-success my-2"
+        onClick={registerBtnOnClick}
+        //You can embbed a state like below to disabled the button
+        //disabled={isUserAgreed}
+      >
+        Register
+      </button>
+    </div>
   );
 }
